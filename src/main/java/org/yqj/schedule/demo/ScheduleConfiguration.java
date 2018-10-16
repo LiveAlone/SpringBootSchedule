cf50bd642e204fc8698d8b4527191956fb5dce61
qijunyao <qijunyao@xiaohongshu.com>
2018-10-16 14:30:34 +0800

    init add all

---
+++ src/main/java/org/yqj/schedule/demo/ScheduleConfiguration.java
@@ -0,0 +1,33 @@
+package org.yqj.schedule.demo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Description:
+ *
+ * @author yaoqijun
+ * @date 2018/10/16
+ * Email: qijunyao@xiaohongshu.com
+ */
+@Configuration
+public class ScheduleConfiguration {
+
+    private static AtomicInteger atomicInteger = new AtomicInteger(0);
+
+    /**
+     * Spring Schedule 定时任务, 默认单线程执行方式， 执行 到时间任务
+     * 提供线程池方式， 并行 cron 任务执行方式
+     * @return
+     */
+    @Bean
+    public ScheduledExecutorService scheduledExecutorService(){
+        return new ScheduledThreadPoolExecutor(6,
+                r -> new Thread(r, "LocalScheduleExecutorThread " + atomicInteger.incrementAndGet()));
+    }
+
+}
--- src/main/java/org/yqj/schedule/demo/scheduled/task/ScheduledTask.java
+++ src/main/java/org/yqj/schedule/demo/scheduled/task/ScheduledTask.java
@@ -21,11 +21,61 @@ public class ScheduledTask {
 
     @Scheduled(fixedRate = 5000)
     public void reportCurrentTime(){
+        try {
+            Thread.sleep(500);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
         log.info("[FixedRate] The time is now {}", dateFormat.format(new Date()));
     }
 
     @Scheduled(cron = "*/10 * * * * *")
-    public void reportTimeCron(){
+    public void reportTimeCron10(){
+        try {
+            Thread.sleep(500);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+        log.info("[ScheduledCron] The time is now {}", dateFormat.format(new Date()));
+    }
+
+    @Scheduled(cron = "*/9 * * * * *")
+    public void reportTimeCron9(){
+        try {
+            Thread.sleep(500);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+        log.info("[ScheduledCron] The time is now {}", dateFormat.format(new Date()));
+    }
+
+    @Scheduled(cron = "*/8 * * * * *")
+    public void reportTimeCron8(){
+        try {
+            Thread.sleep(500);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+        log.info("[ScheduledCron] The time is now {}", dateFormat.format(new Date()));
+    }
+
+    @Scheduled(cron = "*/7 * * * * *")
+    public void reportTimeCron7(){
+        try {
+            Thread.sleep(500);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+        log.info("[ScheduledCron] The time is now {}", dateFormat.format(new Date()));
+    }
+
+    @Scheduled(cron = "*/6 * * * * *")
+    public void reportTimeCron6(){
+        try {
+            Thread.sleep(500);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
         log.info("[ScheduledCron] The time is now {}", dateFormat.format(new Date()));
     }
 
